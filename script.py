b69ab1642571cb2e9520e6c442fcd0a93703dd2c
Jinan MP <jianclash8@gmail.com>
2021-10-30 19:33:03 +0530

    Added a border system and some other changes

--- script.py
+++ script.py
@@ -1,5 +1,5 @@
 import pygame, random
-from pygame.constants import K_DOWN, K_LEFT, K_RIGHT, K_UP
+from pygame.constants import K_DOWN, K_LEFT, K_RIGHT, K_UP, K_a, K_d, K_s, K_w
 
 pygame.init()
 width, heigth = 1300, 700
@@ -7,47 +7,51 @@ width, heigth = 1300, 700
 snake_color = (144,238,144)
 black = (0, 0, 0)
 
-fps = 60
+vel = 3
+fps = 80
 font = pygame.font.Font('freesansbold.ttf', 32)
 
 win = pygame.display.set_mode((width, heigth))
 pygame.display.set_caption('Snake game')
 
-def draw_snake(x, y):
+def draw_snake(x, y, points):
+    # if points != 0:
+    #     snake = pygame.Rect(x, y, 20 * points, 20)
+    # else:
     snake = pygame.Rect(x, y, 20, 20)
     pygame.draw.rect(win, snake_color, snake)
 
 #Moves the snake by updating the x and y values
-def move_snake(x, y, left, right, up, down):
-    if left:
-        x -= 3
-    if right:
-        x += 3
-    if up:
-        y -= 3
-    if down:
-        y += 3
+def move_snake(x, y, left, right, up, down, points):
+    if left and x - vel > 0: 
+        x -= vel
+    if right and x + vel + 20 < width:
+        x += vel
+    if up and y - vel > 0:
+        y -= vel
+    if down and y + vel + 20 < heigth:
+        y += vel
 
     return x, y
 
 #Handles the key presses by turning the apopriate value to true and others to false 
 def handle_keys(key, keys_pressed):
-    if key == K_LEFT:#Left Arrow
+    if key == K_LEFT or key == K_a:#Left Arrow
         for key in keys_pressed: 
             keys_pressed[key] = False
         keys_pressed["left"] = True 
 
-    if key == K_RIGHT:#Right Arrow
+    if key == K_RIGHT or key == K_d:#Right Arrow
         for key in keys_pressed:
             keys_pressed[key] = False
         keys_pressed["right"] = True
 
-    if key == K_UP:#Up Arrow
+    if key == K_UP or key == K_w:#Up Arrow
         for key in keys_pressed:
             keys_pressed[key] = False
         keys_pressed["up"] = True
 
-    if key == K_DOWN:#Down arrow
+    if key == K_DOWN or key == K_s:#Down arrow
         for key in keys_pressed:
             keys_pressed[key] = False
         keys_pressed["down"] = True
@@ -67,7 +71,7 @@ def handle_colision(apple_x, apple_y, x, y):
 
 def display_points(points):
     font = pygame.font.Font('freesansbold.ttf', 20)
-    text = font.render(f"eatan: {points}", True, (255, 255, 255))
+    text = font.render(f"Score: {points}", True, (255, 255, 255))
     textRect = text.get_rect()
     textRect.center = (50, 20)
     win.blit(text, textRect)
@@ -94,8 +98,8 @@ def main():
                 handle_keys(event.key, keys_pressed)
                 
         if run:
-            draw_snake(x, y)
-            x, y = move_snake(x, y, keys_pressed["left"], keys_pressed["right"], keys_pressed["up"], keys_pressed["down"])
+            draw_snake(x, y, points)
+            x, y = move_snake(x, y, keys_pressed["left"], keys_pressed["right"], keys_pressed["up"], keys_pressed["down"], points)
             
             if no_apple:
                 apple_x, apple_y = generate_apples()
